44beb00d0148ad314d490fb1bef8b9f1f0de0df5
Mikko Rautiainen <m_rautiainen@hotmail.com>
2020-01-15 16:26:05 +0100

    estimate probability of copy count

--- src/NetworkFlowCopyCounter.cpp
+++ src/NetworkFlowCopyCounter.cpp
@@ -27,7 +27,17 @@ public:
 		size_t used;
 	};
 	std::vector<Edge> edges;
+	std::unordered_map<int, size_t> nodeStart;
 	size_t maxNode;
+};
+
+class CopycountConfidence
+{
+public:
+	std::unordered_map<int, size_t> copyCount;
+	std::unordered_map<int, double> equalConfidence;
+	std::unordered_map<int, double> biggerConfidence;
+	std::unordered_map<int, double> smallerConfidence;
 };
 
 double contiguityBreakLogOdds()
@@ -116,10 +126,9 @@ FlowGraph buildFlowGraph(const GfaGraph& graph, int numChromosomes, double oneco
 	FlowGraph result;
 	result.maxNode = 0;
 	size_t nextNode = 0;
-	std::unordered_map<int, size_t> nodeStart;
 	for (auto node : graph.nodes)
 	{
-		nodeStart[node.first] = nextNode;
+		result.nodeStart[node.first] = nextNode;
 		size_t kmers;
 		size_t length;
 		std::tie(kmers, length) = getNodeKmersLength(graph, node.first);
@@ -144,11 +153,11 @@ FlowGraph buildFlowGraph(const GfaGraph& graph, int numChromosomes, double oneco
 	{
 		for (auto target : edge.second)
 		{
-			result.edges.emplace_back(NodePos { -2, true }, nodeStart[edge.first.id] * 4 + 1 + (edge.first.end ? 0 : 2), nodeStart[target.id] * 4 + (target.end ? 0 : 2), 0, 1000000, 0);
-			result.edges.emplace_back(NodePos { -2, true }, nodeStart[target.id] * 4 + 1 + (target.end ? 2 : 0), nodeStart[edge.first.id] * 4 + (edge.first.end ? 2 : 0), 0, 1000000, 0);
+			result.edges.emplace_back(NodePos { -2, true }, result.nodeStart[edge.first.id] * 4 + 1 + (edge.first.end ? 0 : 2), result.nodeStart[target.id] * 4 + (target.end ? 0 : 2), 0, 1000000, 0);
+			result.edges.emplace_back(NodePos { -2, true }, result.nodeStart[target.id] * 4 + 1 + (target.end ? 2 : 0), result.nodeStart[edge.first.id] * 4 + (edge.first.end ? 2 : 0), 0, 1000000, 0);
 		}
 	}
-	result.edges.emplace_back(NodePos { -3, true }, -2, -3, -contiguityBreakScore(), numChromosomes, 0);
+	result.edges.emplace_back(NodePos { -3, true }, -2, -3, -contiguityBreakScore(), numChromosomes * 2, 0); //*2 because of forward and reverse flows
 	result.edges.emplace_back(NodePos { -4, true }, -2, -3, contiguityBreakScore(), 1000000, 0);
 	result.edges.emplace_back(NodePos { -4, true }, -3, -1, 0, 1000000, 0);
 	return result;
@@ -181,13 +190,13 @@ private:
 };
 
 // https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
-std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, size_t start, size_t end)
+std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, const size_t start, const size_t end)
 {
 	constexpr double delta = 0.0001;
 	static OffsetedVector<double> distances;
 	static OffsetedVector<std::pair<size_t, bool>> from;
 	static std::vector<size_t> posInPath;
-	distances.assign(graph.maxNode, 3, std::numeric_limits<double>::max());
+	distances.assign(graph.maxNode, 3, std::numeric_limits<double>::infinity());
 	from.assign(graph.maxNode, 3, std::pair<size_t, bool> { -5, true });
 	posInPath.assign(graph.edges.size(), std::numeric_limits<size_t>::max());
 	distances[start] = 0;
@@ -198,7 +207,7 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 		for (size_t i = 0; i < graph.edges.size(); i++)
 		{
 			if ((graph.edges[i].from == start && graph.edges[i].to == end) || (graph.edges[i].from == end && graph.edges[i].to == start)) continue;
-			if (graph.edges[i].used < graph.edges[i].capacity)
+			if (graph.edges[i].used < graph.edges[i].capacity && distances[graph.edges[i].from ]!= std::numeric_limits<double>::infinity())
 			{
 				if (distances[graph.edges[i].to] > distances[graph.edges[i].from] + graph.edges[i].cost + delta)
 				{
@@ -207,7 +216,7 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 					changed = true;
 				}
 			}
-			if (graph.edges[i].used > 0)
+			if (graph.edges[i].used > 0 && distances[graph.edges[i].to] != std::numeric_limits<double>::infinity())
 			{
 				if (distances[graph.edges[i].from] > distances[graph.edges[i].to] - graph.edges[i].cost + delta)
 				{
@@ -224,7 +233,12 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 		}
 		if (distances[end] < 0)
 		{
-			std::cout << "loop to end found ";
+			std::cout << "negative path to end found " << distances[end] << " ";
+			break;
+		}
+		if (distances[start] < 0)
+		{
+			std::cout << "negative loop to start found " << distances[start] << " ";
 			break;
 		}
 	}
@@ -237,6 +251,11 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 		std::cout << "path from end" << std::endl;
 		result.emplace_back(from[end]);
 	}
+	else if (distances[start] < 0)
+	{
+		std::cout << "loop from start" << std::endl;
+		result.emplace_back(from[start]);
+	}
 	else
 	{
 		for (size_t i = 0; i < graph.edges.size(); i++)
@@ -246,6 +265,8 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 			{
 				if (distances[graph.edges[i].to] > distances[graph.edges[i].from] + graph.edges[i].cost + delta)
 				{
+					from[graph.edges[i].to] = std::make_pair(i, true);
+					distances[graph.edges[i].to] = distances[graph.edges[i].from] + graph.edges[i].cost;
 					std::cout << "cycle from edge " << i << "+" << std::endl;
 					result.emplace_back(i, true);
 					break;
@@ -255,6 +276,8 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 			{
 				if (distances[graph.edges[i].from] > distances[graph.edges[i].to] - graph.edges[i].cost + delta)
 				{
+					from[graph.edges[i].from] = std::make_pair(i, false);
+					distances[graph.edges[i].from] = distances[graph.edges[i].to] - graph.edges[i].cost;
 					std::cout << "cycle from edge " << i << "-" << std::endl;
 					result.emplace_back(i, false);
 					break;
@@ -264,8 +287,8 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 	}
 	if (result.size() == 0)
 	{
-		assert(distances[end] >= 0);
-		return result;
+		std::cout << "positive weight path from end" << std::endl;
+		result.emplace_back(from[end]);
 	}
 	assert(result.size() == 1);
 	size_t lastNode = graph.edges[result[0].first].from;
@@ -273,8 +296,8 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 	posInPath[result[0].first] = 0;
 	while (true)
 	{
-		if (result.back().second && graph.edges[result.back().first].from == lastNode) break;
-		if (!result.back().second && graph.edges[result.back().first].to == lastNode) break;
+		if (result.back().second && graph.edges[result.back().first].from == start) break;
+		if (!result.back().second && graph.edges[result.back().first].to == start) break;
 		if (result.back().second)
 		{
 			std::pair<size_t, bool> edge = from[graph.edges[result.back().first].from];
@@ -305,6 +328,16 @@ std::vector<std::pair<size_t, bool>> getNegativeCycle(const FlowGraph& graph, si
 	if (result.back().second) firstNode = graph.edges[result.back().first].from;
 	assert(firstNode == lastNode || (firstNode == start && lastNode == end));
 	assert(result.size() >= 2);
+	if (firstNode == start && lastNode == end)
+	{
+		double pathScore = 0;
+		for (auto edge : result)
+		{
+			if (edge.second) pathScore += graph.edges[edge.first].cost;
+			if (!edge.second) pathScore -= graph.edges[edge.first].cost;
+		}
+		assert(pathScore < 0 == distances[end] < 0);
+	}
 	return result;
 }
 
@@ -372,7 +405,7 @@ void solveFlow(FlowGraph& graph)
 	std::cout << "flow solved " << iteration << std::endl;
 }
 
-void outputFlow(const FlowGraph& flowGraph, const GfaGraph& graph, const std::string& filename)
+void outputFlow(const FlowGraph& flowGraph, const GfaGraph& graph, const CopycountConfidence& confidences, const std::string& filename)
 {
 	std::ofstream file { filename };
 	std::unordered_map<NodePos, int> flowCounts;
@@ -383,14 +416,31 @@ void outputFlow(const FlowGraph& flowGraph, const GfaGraph& graph, const std::st
 			flowCounts[edge.originalNode] += edge.used;
 		}
 	}
-	file << "node,count,kmers,length,coverage" << std::endl;
+	file << "node,count,prob,smallerprob,biggerprob,kmers,length,coverage" << std::endl;
 	for (auto node : graph.nodes)
 	{
 		size_t kmers, length;
 		std::tie(kmers, length) = getNodeKmersLength(graph, node.first);
 		size_t forward = flowCounts[NodePos { node.first, true }];
 		size_t backward = flowCounts[NodePos { node.first, false }];
-		file << node.first << "," << (double)(forward + backward) / 2 << "," << kmers << "," << length << "," << (double)kmers/(double)length << std::endl;
+		file << node.first << "," << confidences.copyCount.at(node.first) / 2 << "," << confidences.equalConfidence.at(node.first) << ",";
+		if (confidences.smallerConfidence.count(node.first) == 1)
+		{
+			file << confidences.smallerConfidence.at(node.first) << ",";
+		}
+		else
+		{
+			file << "-" << ",";
+		}
+		if (confidences.biggerConfidence.count(node.first) == 1)
+		{
+			file << confidences.biggerConfidence.at(node.first) << ",";
+		}
+		else
+		{
+			file << "-" << ",";
+		}
+		file << kmers << "," << length << "," << (double)kmers/(double)length << std::endl;
 	}
 }
 
@@ -417,6 +467,135 @@ double recalcCoverage(const FlowGraph& flowGraph, const GfaGraph& graph)
 	return totalKmers / totalLength;
 }
 
+double shortestPathCost(const FlowGraph& graph, const size_t start, const size_t end)
+{
+	auto path = getNegativeCycle(graph, start, end);
+	assert(path.size() >= 2);
+	size_t lastNode = graph.edges[path[0].first].from;
+	if (path[0].second) lastNode = graph.edges[path[0].first].to;
+	size_t firstNode = graph.edges[path.back().first].to;
+	if (path.back().second) firstNode = graph.edges[path.back().first].from;
+	assert(lastNode == end);
+	assert(firstNode == start);
+	size_t available = 1000000;
+	double cost = 0;
+	for (auto pair : path)
+	{
+		if (pair.second)
+		{
+			assert(graph.edges[pair.first].used < graph.edges[pair.first].capacity);
+			available = std::min(available, graph.edges[pair.first].capacity - graph.edges[pair.first].used);
+			cost += graph.edges[pair.first].cost;
+		}
+		else
+		{
+			assert(graph.edges[pair.first].used > 0);
+			available = std::min(available, graph.edges[pair.first].used);
+			cost -= graph.edges[pair.first].cost;
+		}
+	}
+	assert(available >= 1);
+	return cost;
+}
+
+CopycountConfidence getCopycountConfidences(const FlowGraph& graph)
+{
+	std::unordered_map<NodePos, size_t> nodeStart;
+	std::unordered_map<NodePos, size_t> nodeEnd;
+	std::unordered_map<NodePos, double> minBigger;
+	std::unordered_map<NodePos, double> minSmaller;
+	CopycountConfidence result;
+	for (auto edge : graph.edges)
+	{
+		if (edge.originalNode.id < 0) continue;
+		assert(nodeStart.count(edge.originalNode) == 0 || nodeStart.at(edge.originalNode) == edge.from);
+		assert(nodeEnd.count(edge.originalNode) == 0 || nodeEnd.at(edge.originalNode) == edge.to);
+		nodeStart[edge.originalNode] = edge.from;
+		nodeEnd[edge.originalNode] = edge.to;
+		result.copyCount[edge.originalNode.id] += edge.used;
+		if (edge.used < edge.capacity)
+		{
+			if (minBigger.count(edge.originalNode) == 0 || edge.cost < minBigger.at(edge.originalNode))
+			{
+				minBigger[edge.originalNode] = edge.cost;
+			}
+		}
+		if (edge.used > 0)
+		{
+			if (minSmaller.count(edge.originalNode) == 0 || -edge.cost < minSmaller.at(edge.originalNode))
+			{
+				minSmaller[edge.originalNode] = -edge.cost;
+			}
+		}
+	}
+	for (auto node : nodeStart)
+	{
+		assert(nodeEnd.count(node.first) == 1);
+		if (minBigger.count(node.first) == 1)
+		{
+			auto fwCost = shortestPathCost(graph, nodeEnd.at(node.first), node.second);
+			// assert(fwCost >= -0.01);
+			minBigger[node.first] += fwCost;
+			assert(minBigger[node.first] >= -0.01);
+		}
+		if (minSmaller.count(node.first) == 1)
+		{
+			auto bwCost = shortestPathCost(graph, node.second, nodeEnd.at(node.first));
+			// assert(bwCost >= -0.01);
+			minSmaller[node.first] += bwCost;
+			assert(minSmaller[node.first] >= -0.01);
+		}
+	}
+	for (auto node : result.copyCount)
+	{
+		result.equalConfidence[node.first] = 0;
+		bool hasSmaller = false;
+		bool hasBigger = false;
+		if (minSmaller.count(NodePos { node.first, true }) == 1)
+		{
+			result.smallerConfidence[node.first] = -minSmaller.at(NodePos { node.first, true });
+			hasSmaller = true;
+		}
+		if (minSmaller.count(NodePos { node.first, false }) == 1)
+		{
+			double newSmaller = -minSmaller.at(NodePos { node.first, false });
+			if (hasSmaller)
+			{
+				result.smallerConfidence[node.first] = std::min(result.smallerConfidence[node.first], newSmaller);
+			}
+			else
+			{
+				result.smallerConfidence[node.first] = newSmaller;
+			}
+		}
+		if (minBigger.count(NodePos { node.first, true }) == 1)
+		{
+			result.biggerConfidence[node.first] = -minBigger.at(NodePos { node.first, true });
+			hasBigger = true;
+		}
+		if (minBigger.count(NodePos { node.first, false }) == 1)
+		{
+			double newSmaller = -minBigger.at(NodePos { node.first, false });
+			if (hasBigger)
+			{
+				result.biggerConfidence[node.first] = std::min(result.biggerConfidence[node.first], newSmaller);
+			}
+			else
+			{
+				result.biggerConfidence[node.first] = newSmaller;
+			}
+		}
+		assert(hasSmaller || hasBigger);
+		double totalConfidence = pow(2.7, result.equalConfidence[node.first]);
+		if (hasSmaller) totalConfidence += pow(2.7, result.smallerConfidence[node.first]);
+		if (hasBigger) totalConfidence += pow(2.7, result.biggerConfidence[node.first]);
+		result.equalConfidence[node.first] = pow(2.7, result.equalConfidence[node.first]) / totalConfidence;
+		if (hasSmaller) result.smallerConfidence[node.first] = pow(2.7, result.smallerConfidence[node.first]) / totalConfidence;
+		if (hasBigger) result.biggerConfidence[node.first] = pow(2.7, result.biggerConfidence[node.first]) / totalConfidence;
+	}
+	return result;
+}
+
 int main(int argc, char** argv)
 {
 	std::string inGraph { argv[1] };
@@ -428,5 +607,6 @@ int main(int argc, char** argv)
 	auto flowGraph = buildFlowGraph(graph, numChromosomes, onecopyCoverage);
 	solveFlow(flowGraph);
 	std::cout << "recalced coverage " << recalcCoverage(flowGraph, graph) << std::endl;
-	outputFlow(flowGraph, graph, outputCsv);
+	auto copycountConfidences = getCopycountConfidences(flowGraph);
+	outputFlow(flowGraph, graph, copycountConfidences, outputCsv);
 }
